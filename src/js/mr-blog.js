31804d7f7bfbdd8c386fb945e3d53b6bef975f84
Mark Rabey <wideasleepdad@gmail.com>
2014-05-20 12:51:57 -0400

    added project files

---
+++ src/js/mr-blog.js
@@ -0,0 +1,10 @@
+//jQuery to collapse the navbar on scroll
+var offset_fixed = window.innerHeight - 220;
+var offset_show_down = window.innerHeight - 120;
+$(window).scroll(function() {
+	if($(this).scrollTop() > offset_show_down) {
+		$('#navigation').addClass("navbar-show-fixed");
+	} else {
+		$("#navigation").removeClass("navbar-show-fixed");
+	}
+});
